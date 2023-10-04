97eaa8a1924090c0ffd25532d27acd24570d5252
Kishan Kumar <kumar.kishan@outlook.com>
2023-10-04 19:43:49 +0530

    review changes

--- test/chunk.js
+++ test/chunk.js
@@ -1,38 +1,41 @@
 'use strict'
 
+/* eslint-env mocha */
+
 const assert = require('assert')
 const { chunk } = require('../index')
 
 describe('chunk', () => {
-    it('should return an empty array when input is null', () => {
-        assert.deepStrictEqual(chunk(null, 1), [])
-    })
+  it('should throw a TypeError when size is not a number', () => {
+    assert.throws(() => chunk([1, 2, 3], 'a'), TypeError)
+  })
 
-    it('should return an empty array when size is not a number', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3], 'a'), [])
-    })
+  it('should throw an Error when size is less than 1', () => {
+    assert.throws(() => chunk([1, 2, 3], 0), Error)
+  }
+  )
 
-    it('should return an empty array when size is zero', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3], 0), [])
-    })
+  it('should throw an Error when input is null', () => {
+    assert.throws(() => chunk(null, 1), Error)
+  })
 
-    it('should return an empty array when size is negative', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3], -1), [])
-    })
+  it('should throw when size is negative', () => {
+    assert.throws(() => chunk([1, 2, 3], -1), Error)
+  })
 
-    it('should return an empty array when input is empty', () => {
-        assert.deepStrictEqual(chunk([], 1), [])
-    })
+  it('should return an empty array when input is empty', () => {
+    assert.deepStrictEqual(chunk([], 1), [])
+  })
 
-    it('should return a new array with chunks of the specified size', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 2), [[1, 2], [3, 4], [5]])
-    })  
+  it('should return a new array with chunks of the specified size', () => {
+    assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 2), [[1, 2], [3, 4], [5]])
+  })
 
-    it('should return a new array with chunks of the specified size', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 3), [[1, 2, 3], [4, 5]])
-    })
+  it('should return a new array with chunks of the specified size', () => {
+    assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 3), [[1, 2, 3], [4, 5]])
+  })
 
-    it('should return a new array with chunks of the specified size', () => {
-        assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 1), [[1], [2], [3], [4], [5]])
-    })
+  it('should return a new array with chunks of the specified size', () => {
+    assert.deepStrictEqual(chunk([1, 2, 3, 4, 5], 1), [[1], [2], [3], [4], [5]])
+  })
 })
